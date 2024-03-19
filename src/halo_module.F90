c084554992c47ec1d6b6d0e7a7f2073334c5686c
Jacob Williams <jacobwilliams@users.noreply.github.com>
2024-03-19 10:43:57 -0500

    bugfix

--- src/halo_module.F90
+++ src/halo_module.F90
@@ -2483,7 +2483,7 @@
         ! now propagate the segments:
         ! [export points at a fixed time step]
 !$OMP PARALLEL DO    !...FIRSTPRIVATE(me)
-        do i = 1, nsegs_to_plot
+        do i = 1, size(me%segs)
             call me%segs(isegs(i))%propagate(mode=2,tstep=me%eclipse_dt_step)
         end do
 !$OMP END PARALLEL DO
